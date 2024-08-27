2c5d835ba1d1afc0aa3dce487022993460d975bd
Hudzone <fyodor.blyudov@gmail.com>
2024-08-27 20:57:01 +0300

    KTB-03-04: finished

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,30 @@
 import java.io.File
 
-
 fun main() {
+
+    val dictionary: MutableList<Word> = mutableListOf()
 
     val words: File = File("words.txt")
     val wordsCollection = words.readLines()
 
     wordsCollection.forEach { line ->
-        println(line)
+        val binaryList = line.split("|")
+        val word = Word(binaryList[0], binaryList[1], binaryList[2] ?: 0)
+        dictionary.add(word)
     }
 
+    for (word in dictionary) {
+        println(word)
+    }
+
+}
+
+data class Word(
+    val word: String,
+    val translation: String,
+    private var correctAnswerCounter: Comparable<*> = 0,
+) {
+    override fun toString(): String {
+        return "Слово: $word\nПеревод: $translation\nКоличество верных ответов: $correctAnswerCounter\n"
+    }
 }
