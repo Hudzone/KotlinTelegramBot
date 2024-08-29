4647f06bf931107da25675bd48d962a173b559c5
Hudzone <fyodor.blyudov@gmail.com>
2024-08-29 19:48:52 +0300

    KTB-05: finished

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ import java.io.File
 
 fun main() {
 
+    mainMenu()
+
     val dictionary: MutableList<Word> = mutableListOf()
 
     val words: File = File("words.txt")
@@ -28,4 +30,42 @@ data class Word(
     override fun toString(): String {
         return "Слово: $word\nПеревод: $translation\nКоличество верных ответов: $correctAnswerCounter\n"
     }
+}
+
+fun mainMenu() {
+
+    while (true) {
+
+        println(
+            """
+            1 - Учить слова
+            2 - Статистика
+            0 - Выход
+        """.trimIndent()
+        )
+
+        print("Ваш выбор: ")
+        val userInput: Int = try {
+            readln().toInt()
+        } catch (e: NumberFormatException) {
+            println("Неверный ввод")
+            continue
+        }
+
+        when (userInput) {
+            1 -> println("Тут будет функционал тренажера")
+            2 -> println("Тут будет статистика")
+            0 -> {
+                println("Завершение программы")
+                break
+            }
+
+            else -> {
+                println("Неверный ввод")
+                continue
+            }
+        }
+
+    }
+
 }
